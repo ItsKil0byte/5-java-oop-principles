cf15de5ede3432fef1cb291d15d2b75fff55ba96
Alexander <itskilobyte@gmail.com>
2024-10-16 11:03:21 +0500

    Completed 1st task.

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,15 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+        Logger logger = new Logger("FirstLogger");
+        logger.setLevel(LoggerLevel.INFO);
 
+        logger.info("Это информационное сообщение");
+        logger.debug("Это сообщение не должно быть видно");
+
+        logger.warning("Это сообщение об предупреждении: %s%n", "Завтра защита лабы.");
+
+        Logger sameLogger = Logger.getLogger("FirstLogger");
+        System.out.println(logger == sameLogger);
     }
 }
